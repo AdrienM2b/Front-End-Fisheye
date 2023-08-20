60c061f324ab567320f69e9ab96219552026aa7c
AdrienM2b <macbook@macbook-pro-de-macbook.home>
2023-08-20 18:21:59 +0200

    affichage input console

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,5 +1,17 @@
 
 const contactModal = document.getElementById("contact_modal");
+document.querySelector('.contact_button').onclick = function() {displayModal()}
+const contactButton = document.querySelector('#contact-modal__button')
+
+contactButton.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const nom = document.querySelector('#nom').value
+    const message = document.querySelector('#message').value
+    const prenom = document.querySelector('#prenom').value
+    console.log(prenom, nom, message)
+    
+})
 
 function displayModal() {
 	contactModal.style.display = "block";
@@ -9,6 +21,7 @@ function displayModal() {
         }
     })
 }
+document.querySelector('.close_button').onclick = function() {closeModal()}
 
 function closeModal() {
     contactModal.style.display = "none";
