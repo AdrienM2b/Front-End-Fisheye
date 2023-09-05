61bb8d1a009f03a0e532fc5815af520e3393107a
AdrienM2b <macbook@MacBook-Pro-de-MacBook.local>
2023-09-05 08:12:24 +0200

    ajout de commentaire et modification mineure

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,31 +1,28 @@
 import { photographerFactory } from '../factories/photographer.js'
 
 async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let reponse = await fetch('data/photographers.json');
-        let photographers = (await reponse).json();
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return photographers;
-    }
+        let reponse = await fetch('data/photographers.json')
+        let photographers = (await reponse).json()
+        return photographers
+}
 
-    
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+// deployer la factory photographe pour afficher la page d'accueil   
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section")
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    }
-    
-   async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    }
-    
-    init();
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer)
+        const userCardDOM = photographerModel.getUserCardDOM()
+        photographersSection.appendChild(userCardDOM)
+    })
+}
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers()
+    displayData(photographers)
+}
+
+init()
     
     
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,67 +1,64 @@
-import { displayMedia } from "../utils/displayMedia.js";
+import { displayMedia } from "../utils/displayMedia.js"
 import { photographerFactory } from "../factories/photographer.js"
-import { sortByButtons } from "../utils/sortByButton.js";
+import { sortByButtons } from "../utils/sortByButton.js"
 
 //Mettre le code JavaScript lié à la page photographer.html
 let params = new URL(document.location).searchParams;
-let photographerId = params.get("id");
+let photographerId = params.get("id")
 const main = document.querySelector('#main')
 
 async function getData() {
-    let reponse = await fetch('data/photographers.json');
-    let data = (await reponse).json();
-    // bien retourner les tableaux une fois récupéré
-    return data;
+    let reponse = await fetch('data/photographers.json')
+    let data = (await reponse).json()
+    return data
 }
 
 
 async function initPhotographer(){
-    const { photographers, media } = await getData();
+    const { photographers, media } = await getData()
     // trouver le nom du photographe pour les images
-    const foundPhotographer = photographers.find(element => element.id == photographerId);
+    const foundPhotographer = photographers.find(element => element.id == photographerId)
     // filtrer les image du photographe
-    const photo = media.filter(m => m.photographerId === parseInt(photographerId));
+    const photo = media.filter(m => m.photographerId === parseInt(photographerId))
 
     // initialiser les fonctions 
     sortByButtons(photo)
     displayFactories(foundPhotographer, photo)
     containerLikePrice(foundPhotographer)
 }
-initPhotographer();
+initPhotographer()
 
 
 
 async function displayFactories(foundPhotographer, photo){
-    // fabrication du header
     //selection Header
-    const photographerHeader = document.querySelector('.photograph-header');
-    const photographerHeaderModel = photographerFactory(foundPhotographer);
-    const userHeaderDOM = photographerHeaderModel.userImageProfil();
-    const userContentDOM = photographerHeaderModel.userContent();
+    const photographerHeader = document.querySelector('.photograph-header')
+    const photographerHeaderModel = photographerFactory(foundPhotographer)
+    const userHeaderDOM = photographerHeaderModel.userImageProfil()
+    const userContentDOM = photographerHeaderModel.userContent()
     
     // créer le contenu du header
-    photographerHeader.appendChild(userContentDOM);
-    photographerHeader.appendChild(userHeaderDOM);
+    photographerHeader.appendChild(userContentDOM)
+    photographerHeader.appendChild(userHeaderDOM)
 
     // ajouter la fonction pour afficher les photos
     displayMedia(photo)
-
 }
 
 async function containerLikePrice(foundPhotographer){
     //affichage du Prix et nbr de like du photographe
-    const boxPrice = document.createElement('div');
+    const boxPrice = document.createElement('div')
     boxPrice.classList.add('boxPrice-photographer')
     // recuperer le prix 
     const photographPrice = document.createElement('p')
     photographPrice.textContent = `${foundPhotographer.price} €/jour`
 
     // nombre de likes
-    const likeContainer = document.createElement('div');
-    likeContainer.classList.add('total-likes_container');
+    const likeContainer = document.createElement('div')
+    likeContainer.classList.add('total-likes_container')
     
-    const totalLikes = document.createElement('p');
-    totalLikes.classList.add('total-likes');
+    const totalLikes = document.createElement('p')
+    totalLikes.classList.add('total-likes')
     totalLikes.textContent = '0'
 
     // ajout de l'icone
