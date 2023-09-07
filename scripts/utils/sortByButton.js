850e679e7cd9d0dd4851b0611639a9fd4e9f64ae
AdrienM2b <macbook@MacBook-Pro-de-MacBook.local>
2023-09-07 14:27:17 +0200

    changement du dropdown

--- scripts/utils/sortByButton.js
+++ scripts/utils/sortByButton.js
@@ -5,118 +5,6 @@ function compareStrings(a, b) {
     return (a < b) ? -1 : (a > b) ? 1 : 0
 }
 const main = document.querySelector('main')
-
-function buttonSort(){
-    // Container title + dropdown
-    const container = document.createElement('div')
-    container.classList.add('sort-by')
-    
-    // Titre 'trier par'
-    const title = document.createElement('p')
-    title.textContent = 'Trier par'
-    title.classList.add('sort-by__title')
-    
-    // Container du dropdown
-    const buttonContainer = document.createElement('div')
-    buttonContainer.classList.add('sort-by__container__button')
-
-    // Container des options
-    const listOptions = document.createElement('ul')
-    listOptions.classList.add('sort-by__list')
-    listOptions.classList.add('hide')
-    listOptions.id = 'selectMenu'
-
-    // Options
-    const options = ["Date", "Popularité", "Titre"]
-
-    options.forEach(optionText => {
-        const option = document.createElement('li')
-        option.classList.add('sort-by__option')
-        option.textContent = optionText
-        option.setAttribute('value', optionText.toLowerCase())
-        listOptions.appendChild(option)
-    })
-
-    // Liaison des elements entre eux
-    container.appendChild(title)
-    container.appendChild(buttonContainer)
-    buttonContainer.appendChild(listOptions)
-
-    return container
-}
-
-function sortByButtons(photo){
-    main.appendChild(buttonSort())
-
-    // Récupérez les éléments DOM
-    const selectMenu = document.getElementById('selectMenu')
-    const selectLien = document.querySelectorAll('.sort-by__option')
-    
-    // cacher les deux derniers elemetns de la liste
-    for (let i = selectLien.length - 1; i >= selectLien.length - 2; i--) {
-        selectLien[i].classList.add('hidden')
-    }
-
-    // Sélectionnez le menu déroulant et ajoutez un gestionnaire d'événements pour le clic
-    selectMenu.addEventListener('click', function() {
-        // (masquer le menu)Si le menu est déjà affiché
-        if (selectMenu.classList.contains('show')) {
-            selectMenu.classList.toggle('hide')
-            selectMenu.classList.remove('show')
-            closeDropdown()  
-        //(afficher le menu)   
-        } else if(selectMenu.classList.contains('hide')){
-            selectMenu.classList.toggle('show')
-            selectMenu.classList.remove('hide')
-            showDropdown()
-            // afficher le menu et enlever la classe hidden des éléments cachés
-            selectLien.forEach(li => {
-                li.classList.remove('hidden')
-                li.classList.remove('selected')
-                li.addEventListener('click', function(event) {
-                    const selectedValue = event.target.getAttribute('value')
-                    // Appliquer la classe 'selected' à l'élément cliqué et 'hidden' aux autres
-                    selectLien.forEach(li => {
-                        if (li.getAttribute('value') === selectedValue) {
-                            li.classList.add('selected')
-                            // mettre à zero la gallerie
-                            const gallery = document.querySelector('.gallery')
-                            main.removeChild(gallery)
-                            // choisir le mode de tri
-                            switch (selectedValue) {
-                                case 'popularité':
-                                    sortByPopularity(photo)
-                                break
-                                case 'date':
-                                    sortByDate(photo)
-                                break
-                                case 'titre':
-                                    sortByTitre(photo)
-                                break
-                            }
-                            displayMedia(photo)
-                        } 
-                    })
-                })
-            })
-        }
-    })
-    function closeDropdown(){
-        selectLien.forEach(li => {
-            if(!li.classList.contains('selected')){
-                li.classList.toggle('hidden')
-                li.classList.remove('selected')
-            }
-        })
-    }
-    function showDropdown(){
-        selectLien.forEach(li => {
-            if(li.classList.contains('hidden')){
-                li.classList.remove('hidden')
-            }
-        })
-    }
-}
         
 function sortByPopularity(photo){
         photo.sort(function (a,b){return a.likes-b.likes})
@@ -129,6 +17,100 @@ function sortByTitre(photo){
         return compareStrings(a.title, b.title)
     })
 }
+
+function sortByButtons(photo){
+    const elements = ['Date', 'Popularité', 'Titre']
+    // element regroupant les 2 container le parapgraphe et le bouton
+    const container = document.createElement('div')
+    container.classList = "sort_by-container"
+
+    // conteneur tri + boite
+    const parapgraph = document.createElement('p')
+    parapgraph.textContent = 'Trier par'
+
+    // Le conteneur du bouton + de la liste
+    const inputSelection = document.createElement('div')
+    inputSelection.classList = 'bouton_input'
+    inputSelection.addEventListener("click", toggleDropdown)
+
+    // conteneur du placeholder + de la fleche
+    const containerArrowInput = document.createElement('div')
+    containerArrowInput.classList = 'container_arrow_input'
+
+    // fleche vers le bas
+    const arrowDown = document.createElement('span')
+    arrowDown.innerHTML = '&lt;'
+    arrowDown.classList.add('arrow', 'down')
+
+    // le titre du bouton (par defaut 'Date')
+    const placeholder = document.createElement("p")
+    placeholder.textContent = elements[0];
+    placeholder.classList.add('placeholder')
+
+    container.appendChild(parapgraph)
+    container.appendChild(inputSelection)
+    containerArrowInput.appendChild(arrowDown)
+    containerArrowInput.appendChild(placeholder)
+    inputSelection.appendChild(containerArrowInput)
+    main.appendChild(container)
+
+    // la div contenant les choix de tri
+    const structure = document.createElement("div")
+    structure.classList.add("structure", "hide")
+
+    // Creation de chaque choix
+    elements.forEach(uniqueElements => {
+        const option = document.createElement("div")
+        // On ajoute un ecouteur pour chaque choix de tri
+        option.addEventListener("click", (event) => selectOption(event))
+        option.textContent = uniqueElements
+        structure.appendChild(option)
+    })
+
+    inputSelection.appendChild(structure)
+
+    // afficher le dropdown
+    function toggleDropdown() {
+        const dropdown = document.querySelector(".structure")
+        dropdown.classList.toggle("hide")
+        placeholder.classList.toggle("hide")
+      
+        const input = document.querySelector(".bouton_input")
+        input.classList.toggle("input__active")
+        const arrow = document.querySelector('.arrow')
+        arrow.classList.toggle('up')
+    }
+
+    // afficher l'elements selectionné et trier les photos
+    function selectOption(event) {
+        event.stopPropagation()
+        const elementsSelectionne = event.target.innerHTML
+        console.log(elementsSelectionne)
+        const text = document.querySelector('.placeholder')
+
+        text.textContent = elementsSelectionne
+        text.classList.add('input__selected')
+        toggleDropdown()
+
+        const gallery = document.querySelector('.gallery')
+        main.removeChild(gallery)
+        // choisir le mode de tri
+        switch (elementsSelectionne.toLowerCase()) {
+            case 'popularité':
+                sortByPopularity(photo)
+            break
+            case 'date':
+                sortByDate(photo)
+            break
+            case 'titre':
+                sortByTitre(photo)
+            break
+        }
+        displayMedia(photo)
+    }  
+}
+
+
 
 
 export { sortByButtons }
